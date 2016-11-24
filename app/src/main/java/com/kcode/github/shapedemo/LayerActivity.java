23788e5ae3218c6e1cb855c3f98242067b380152
caikai <caikai@guahao.com>
2016-11-24 21:11:09 +0800

    1.add Nine Drawable 2.add Layer Drawable 3.add Level Drawable

--- app/src/main/java/com/kcode/github/shapedemo/LayerActivity.java
+++ app/src/main/java/com/kcode/github/shapedemo/LayerActivity.java
@@ -1,5 +1,7 @@
 package com.kcode.github.shapedemo;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,5 +9,9 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class LayerActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layer);
+    }
 }
--- app/src/main/java/com/kcode/github/shapedemo/LevelActivity.java
+++ app/src/main/java/com/kcode/github/shapedemo/LevelActivity.java
@@ -1,11 +1,21 @@
 package com.kcode.github.shapedemo;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by caik on 2016/11/23.
  */
 
 public class LevelActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
 
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.getDrawable().setLevel(2);
+    }
 }
